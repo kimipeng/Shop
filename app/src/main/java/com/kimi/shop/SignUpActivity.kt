ed8ff90a7d568985c5336cd17ee90b246cb28fdd
kimipeng <pon0306@gmail.com>
2019-04-23 15:01:04 +0800

    導入Firebase，會員註冊

---
+++ app/src/main/java/com/kimi/shop/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.kimi.shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        signup.setOnClickListener {
+            val sEmail = email.text.toString()
+            val sPassword =  password.text.toString()
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(sEmail, sPassword)
+        }
+    }
+}
